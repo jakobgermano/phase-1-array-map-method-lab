36ae2c42eeea53979763623055cc0fd370dd6b2a
jakob <jakob.germano@gmail.com>
2021-06-21 19:49:50 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -10,7 +10,31 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+const titleCased = () =>  {
+  return tutorials.map(words => words.split(' ').map(cap => 
+    cap.charAt(0).toUpperCase() + cap.slice(1)).join(' '))
+  }
+  
 
-const titleCased = () => {
-  return tutorials
-}
+//const titleCased = () => {
+  //const cap = tutorial.split(" ")
+  //cap.map(newtutorial => first[0].toUpperCase() + newtutorial.substring(1)).join(" ")
+    //return newTutorial
+//}
+
+//const titleCased = tutorials => {
+  //tutorials.map(function (element) {
+   //const words = element.split(" ");
+   // words.map((word) => {
+    //  return word[0].toUpperCase() + word.substring(1);
+   // }).join(" ");
+////  })
+
+//const titleCased = () => { 
+  //const cap = tutorials
+  //cap.map(newTutorial => newTutorial[0].toUpperCase(titleCased))
+ // return titleCased 
+//}
+//create function that iretiates over first letter of each word on each line//
+//use map to return new array that has all new words
+//
